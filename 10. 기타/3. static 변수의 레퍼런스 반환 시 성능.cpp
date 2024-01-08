27e14d388a8ed267d83fd3cf4dd5ddefb34ebed6
Kyungbin Cho <diesuki4@gmail.com>
2024-01-08 22:43:46 +0900

    10. 기타/3. static 변수의 레퍼런스 반환 시 성능 추가
    static 구조체/클래스의 초기화 어셈블리

--- 10. 기타/3. static 변수의 레퍼런스 반환 시 성능.cpp
+++ 10. 기타/3. static 변수의 레퍼런스 반환 시 성능.cpp
@@ -1,3 +1,6 @@
+/*
+ * https://stackoverflow.com/questions/59985569/should-i-use-a-barrier-while-accessing-statically-initialized-variable#answer-59986089
+ */
 #include <iostream>
 
 using namespace std;
@@ -6,29 +9,61 @@ using namespace std;
  * static 변수의 레퍼런스 반환 시 성능
  */
 
-class Class { public: int num = 10; };
+struct Class
+{
+    int data;
 
-/*
- * 레퍼런스가 값 타입보다 항상
+    Class() : data(5) { }
+};
+
+/* 전역 변수는 프로그램 실행 시에 한꺼번에 초기화된다. */
+Class a;
+
+/* 레퍼런스가 값 타입보다 항상
  * 반환 비용이 적은 것은 아니다.
- * 
+ *
  * 현재 Class 객체의 크기는 4바이트지만,
- * 
+ *
  * x64 시스템에서는
- * 레퍼런스는 오히려 8바이트가 된다.
- */
+ * 레퍼런스는 오히려 8바이트가 된다. */
 Class& getClass()
 {
-    /*
-     * 함수 내의 static 변수는
-     * 처음 이 문장에 도달했을 때만
-     * 초기화된다.
-     * 
+    /* 기본 타입 static 변수는
+     * 프로그램 실행 시에 초기화되지만,
+     *
+     * 기본 타입이 아닌 구조체, 클래스 등은
+     * 처음 이 문장에 도달했을 때만 초기화된다.
+     *
      * 그래서, 첫 실행인지 확인하는
      * 보이지 않는 로직이 들어가기 때문에
-     * 
-     * 비용이 발생한다.
-     */
+     *
+     * 코드 양도 커지고 추가 비용도 발생한다. */
+
+    //         이미 초기화됐는지 확인
+    //         cmp     byte ptr [$guard variable for c], 0
+    //         jne     .LBB_4
+    // 
+    //         멀티스레드 환경에서 한 스레드만 초기화를 진행하기 위한 락
+    //         lea     rdi, byte ptr [$guard variable for c]
+    //         call    guard_acquire()
+    //         cmp     eax, 0
+    //         je      .LBB_4
+    // 
+    //         생성자 호출 후 락 해제
+    //         lea     rdi, byte ptr [$a]
+    //         call    AAA::AAA()
+    //         lea     rdi, byte ptr [$guard variable for c]
+    //         call    guard_release()
+    // 
+    // .LBB_4  함수 로직
+    //         mov     dword ptr [rip + square()::a], 9
+    //         ...
+    // 
+    //         static 변수 초기화 중 예외 발생 시 Stack Unwinding 을 위한 코드
+    //         lea     rdi, byte ptr [$guard variable for c]
+    //         call    guard_abort()
+    //         lea     rdi, dword ptr [rbp - 8]
+    //         call    unwind_resume()
     static Class c;
 
     /*
@@ -46,10 +81,10 @@ int main(int argc, char* argv[])
     /*
      * 레퍼런스는 실제로는 컴파일러에 의해
      * 포인터로 번역된다.
-     * 
+     *
      * 따라서, 역참조 비용이 따른다.
      */
-    cout << c.num << endl;
+    cout << c.data << endl;
 
     return 0;
 }
--- 10. 기타/5 임시 객체와 상수 참조자.cpp
+++ 10. 기타/5 임시 객체와 상수 참조자.cpp
@@ -35,12 +35,12 @@ int main(int argc, char* argv[])
      * 하지만, 값 불변을 보장할 수 있는 상수 레퍼런스는 가능하다.
      * 
      * 이때는 상수 레퍼런스의 생명주기가 끝날 때,
-     * 
+     *
      * 임시 객체도 함께 소멸한다.
      */
-    const Widget& ref = Widget(10);
+    const Widget& cref = Widget(10);
 
-    cout << "임시 객체 Widget(10) 은 ref 가 해제될 때, 함께 소멸한다." << endl;
+    cout << "임시 객체 Widget(10) 은 cref 가 해제될 때, 함께 소멸한다." << endl;
 
     return 0;
 }
