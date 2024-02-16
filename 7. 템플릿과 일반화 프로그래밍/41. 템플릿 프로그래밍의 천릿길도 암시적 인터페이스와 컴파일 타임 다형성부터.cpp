82176091705a460eede7a7c1066e5d0bbdaead4f
Kyungbin Cho <diesuki4@gmail.com>
2024-02-16 21:51:10 +0900

    19일차
    295p ~ 306p

---
+++ 7. 템플릿과 일반화 프로그래밍/41. 템플릿 프로그래밍의 천릿길도 암시적 인터페이스와 컴파일 타임 다형성부터.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 템플릿 프로그래밍의 천릿길도 암시적 인터페이스와 컴파일 타임 다형성부터
+ * 
+ * 정적(컴파일 타임) 다형성
+ * - 함수 오버로딩
+ * - 템플릿
+ * 
+ * 동적(런타임) 다형성
+ * - 가상 함수
+ */
+
+class Widget
+{
+public:
+    Widget() {}
+
+    virtual size_t size() const { return 10; }
+    virtual void normalize() const {}
+
+    void swap(Widget& other) {}
+    bool operator!= (const Widget& rhs) { return true; }
+
+    virtual ~Widget() {}
+};
+
+Widget someWidget;
+
+/* 일반 함수는 타입의 인터페이스에 따라 함수를 작성하면 된다.
+ * 이 때의 중점은 함수의 시그니처다. */
+void doProcessing(Widget& w)
+{
+    if (10 < w.size() && w != someWidget)
+    {
+        Widget temp(w);
+        // 소스 코드에 명시적으로 드러나는 명시적 인터페이스
+        temp.normalize();
+        // virtual 함수이므로 런타임 다형성
+        temp.swap(w);
+    }
+}
+
+Widget someT;
+
+/* 함수 템플릿은 함수 내용에 따라 타입의 인터페이스를 작성해줘야 한다.
+ * 이 떄의 중점은 본문의 표현식이다. */
+template <typename T>
+void doProcessingT(T& t)
+{
+    if (10 < t.size() && t != someT)
+    {
+        T temp(t);
+        // 컴파일을 위해 T 타입이 지원해야 하는 암시적 인터페이스
+        temp.normalize();
+        /* 템플릿은 컴파일 타임에 인스턴싱되는데, 이때 타입에 따라
+         * 호출되는 함수가 달라지기 때문에 컴파일 타임 다형성이라고 한다. */
+        temp.swap(t);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    
+
+    return 0;
+}
---
+++ 7. 템플릿과 일반화 프로그래밍/42. typename의 두 가지 의미를 제대로 파악하자.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <type_traits>
+
+using namespace std;
+
+/*
+ * typename의 두 가지 의미를 제대로 파악하자
+ */
+
+// 클래스만 올 때는 class
+template <class T>
+class Button;
+// 모든 타입이 올 때는 typename
+template <typename T>
+class Widget;
+
+template <typename T>
+void Func()
+{
+    // 비의존 이름 (Non-dependent or Independent name)
+    int value = 0;
+    // 의존 이름 (Dependent name)
+    vector<T> vec;
+    /* 중첩 의존 이름(Nested dependent name)
+     * 
+     * 컴파일러는 typename 키워드를 붙여주지 않는 한,
+     * 중첩 의존 이름이 타입이라고 가정하지 않는다. */
+    typename T::const_iterator iter;
+};
+
+template <typename T>
+class Base
+{
+public:
+    class Nested
+    {
+    public:
+        Nested(int x) {}
+    };
+};
+
+template <typename T>
+// 1. Base 클래스 목록에 있거나
+class Derived : public Base<T>::Nested
+{
+    // 2. 멤버 이니셜라이저 리스트에 있을 때는, 용도가 명확하므로 쓰지 않아도 된다.
+    Derived(int x) : Base<T>::Nested(x)
+    {
+        // 여기서는 써 줘야 한다.
+        typename Base<T>::Nested temp;
+    }
+};
+
+template <typename IterT>
+void workWithIterator(IterT iter)
+{
+    typedef typename iterator_traits<IterT>::value_type value_type;
+}
+
+int main(int argc, char* argv[])
+{
+    
+
+    return 0;
+}
